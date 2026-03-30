a56574460a38e4888303cb9e530c76bffd8e24cd
felipepassos068 <felipepassos5555@gmail.com>
2026-03-30 14:56:31 -0300

    limpeza do arquivo tests

--- vendas/tests.py
+++ vendas/tests.py
@@ -1,16 +1,2 @@
 from django.test import TestCase
 
-class Produto(models.Model):
-    nome = models.CharField(max_length=100)
-    categoria = models.CharField(max_length=50)
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
-    quantidade_estoque = models.IntegerField()
-
-    def __str__(self):
-        return self.nome
-    
-class Vendedor(models.Model):
-    nome = CHarField(max_length=100)
-
-    def __str__(self):
-       return self.nome
