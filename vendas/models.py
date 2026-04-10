8cc1906de7b2b49ef9c187924885c1e7364226aa
felipepassos068 <felipepassos5555@gmail.com>
2026-04-10 11:53:58 -0300

    projeto completo

--- vendas/models.py
+++ vendas/models.py
@@ -19,8 +19,8 @@ class Vendedor(models.Model):
 
 
 class Venda(models.Model):
-    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
-    vendedor = models.ForeignKey(Vendedor, on_delete=models.CASCADE)
+    produto = models.ForeignKey('Produto', on_delete=models.CASCADE)
+    vendedor = models.ForeignKey('Vendedor', on_delete=models.CASCADE)
     quantidade = models.IntegerField()
     valor_unitario = models.DecimalField(max_digits=10, decimal_places=2)
     valor_total = models.DecimalField(max_digits=10, decimal_places=2)
@@ -35,17 +35,23 @@ class Venda(models.Model):
 
         if self.quantidade <= 0:
             raise ValidationError("Quantidade inválida")
+        
+def save(self, *args, **kwargs):
+    print("ESTOQUE:", self.produto.quantidade_estoque)
+    print("QUANTIDADE:", self.quantidade)
 
-        if self.quantidade > self.produto.quantidade_estoque:
-            raise ValidationError("Estoque insuficiente")
 
-    def save(self, *args, **kwargs):
-        if not self.pk:
-            self.valor_unitario = self.produto.preco
-            self.valor_total = self.quantidade * self.valor_unitario
+    self.valor_unitario = self.produto.preco
 
-            self.produto.quantidade_estoque -= self.quantidade
-            self.produto.save()
+ 
+    if self.quantidade > self.produto.quantidade_estoque:
+        raise ValueError("Estoque insuficiente")
 
-        super().save(*args, **kwargs)
+    self.valor_total = self.quantidade * self.valor_unitario
+
+    if not self.pk:
+        self.produto.quantidade_estoque -= self.quantidade
+        self.produto.save()
+
     
+    super().save(*args, **kwargs)        
--- vendas/urls.py
+++ vendas/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+
     path('produtos/', views.lista_produtos, name='lista_produtos'),
-    path('produtos/buscar/', views.buscar_produtos, name='buscar_produtos'),
     path('produtos/criar/', views.criar_produto, name='criar_produto'),
     path('produtos/editar/<int:id>/', views.editar_produto, name='editar_produto'),
     path('produtos/deletar/<int:id>/', views.deletar_produto, name='deletar_produto'),
+    path('vendas/', views.lista_vendas, name='lista_vendas'),
+    path('vendas/criar/', views.criar_venda, name='criar_venda'),
+    path('editar-venda/<int:id>/', views.editar_venda, name='editar_venda'),
+    path('api/dashboard/', views.dados_dashboard, name='dados_dashboard'),
+    path('dashboard/', views.dashboard, name='dashboard'), 
 ]
